9ebe14fa7b92026f4e721ab17da3ab5b792b6da6
Marlin Kreatif <marlina.kreatif@gmail.com>
2020-10-02 13:15:59 +0700

    adding reactjs to electron-forge app

---
+++ src/renderer.jsx
@@ -0,0 +1,5 @@
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+console.log("Loaded React.");
+ReactDOM.render(<div>Test.</div>, document.getElementById("root"));
