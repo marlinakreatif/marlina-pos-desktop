043fcd0f4b5ea4095c874d8bd307da03a3961cb4
Marlin Kreatif <marlina.kreatif@gmail.com>
2020-10-02 16:42:17 +0700

    memperbaiki masalah tidak dapat merender map

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,13 +3,26 @@ import { GET_ALL_PRODUCT } from "../constants/ipc.constant";
 import sendAsync from "../ipc-electron/ipc-rendere";
 
 export default class App extends Component {
+  state = {
+    products: [],
+  };
   componentDidMount() {
     sendAsync({ action: GET_ALL_PRODUCT, payload: null }).then((result) =>
-      console.log("RESULT ", result)
+      this.setState({ products: result })
     );
   }
 
   render() {
-    return <div>Selamat Datang Pekok</div>;
+    const { products } = this.state;
+    return (
+      <div>
+        <span>Selamat Datang Pekok Aku ingi selalu bersama kamu</span>
+        <div>
+          {products.map((data, index) => {
+            return <span key={index}>{data.nama}</span>;
+          })}
+        </div>
+      </div>
+    );
   }
 }
